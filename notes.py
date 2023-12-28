c302186239414ee4b4972df732d6f463085c55e4
Semih Yılmaz <semihyilmaz2021@gmail.com>
2023-12-28 19:14:14 +0300

    add notes changes

--- notes.py
+++ notes.py
@@ -4,10 +4,10 @@
 # git branch -a  - show all branches
 # git branch   - shows which branch you are in
 # git branch -m <old branch name> <new branch name>
-# git checkout <branch name>
+# git checkout <branch name> - changes branches
 # git add -a   - adding all changes to staging area
 # git commit -m 'comment'  - saving all added changes to staging area in your local
 # git push -u origin <branch name> 
 # git pull  #pull files from remote to local
-# git merge
+# git merge  <branch to be merged with current branch>
 
